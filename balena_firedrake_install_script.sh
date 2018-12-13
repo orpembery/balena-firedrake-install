89c7b1c1c734ac1049a9346f98ad0062fa1626f0
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-12-13 13:32:09 +0000

    Tidied up real install script.

--- balena_firedrake_install_script.sh
+++ balena_firedrake_install_script.sh
@@ -15,7 +15,7 @@ module load cmake/3.5.2
 module load intel/compiler/64/18.0.128
 module load slurm
 module load zlib
-#module load hdf5/gcc/1.8.17
+
 
 # Set main to be working directory
 MAIN=`pwd`
@@ -34,18 +34,15 @@ unset PETSC_DIR
 cd ./petsc
 
 # Configure PETSc
-# remove --prefix??
-# Last two options weren't in Jack's script, but were passed by the complex install script
-./configure --with-zlib-dir=/apps/zlib/1.2.8 --with-shared-libraries=1 --with-debugging=0 --with-c2html=0 --with-cc=mpiicc --with-cxx=mpiicpc --with-fc=mpiifort --download-fblaslapack --download-eigen --with-fortran-bindings=0 --download-chaco --download-metis --download-parmetis --download-scalapack --download-hypre --download-mumps --download-netcdf --download-hdf5 --download-pnetcdf # --download-exodusii --with-scalar-type=complex
+./configure --with-zlib-dir=/apps/zlib/1.2.8 --with-shared-libraries=1 --with-debugging=0 --with-c2html=0 --with-cc=mpiicc --with-cxx=mpiicpc --with-fc=mpiifort --download-fblaslapack --download-eigen --with-fortran-bindings=0 --download-chaco --download-metis --download-parmetis --download-scalapack --download-hypre --download-mumps --download-netcdf --download-hdf5 --download-pnetcdf
 
 # Build PETSc
 make -j 16 PETSC_DIR=${MAIN}/petsc PETSC_ARCH=arch-python-linux-x86_64 all
 
-# REMOVE???
-#make -j 17 PETSC_DIR=${MAIN}/petsc PETSC_ARCH=arch-python-linux-x86_64 install
+
 # CHECK???
 make -j 16 PETSC_DIR=${MAIN}/petsc PETSC_ARCH=arch-python-linux-x86_64 check
-#make -j 17 PETSC_DIR=${MAIN}/petsc PETSC_ARCH=arch-python-linux-x86_64 streams # This checks the scaling and is optional
+
 
 
 # Set PETSc directory
@@ -63,8 +60,6 @@ export PYTHON_DIR=${MAIN}/python3
 cd ./python3
 
 # Configure
-# try with --with-tcltk-includes='-I/apps/python/intel/2018.1.023/intelpython3/include'
-# and --with-tcltk-libs='-L/apps/python/intel/2018.1.023/intelpython3/lib -ltcl8.6 -ltk8.6'
 ./configure --enable-shared --enable-ipv6 --with-ensurepip=yes --prefix=${PYTHON_DIR} CPPFLAGS=-I${PYTHON_DIR}/include LDFLAGS="-L${PYTHON_DIR}/lib -Wl,-rpath=${PYTHON_DIR}/lib,--no-as-needed" CFLAGS="-Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -fstack-protector -O3 -fpic -fPIC" PKG_CONFIG_PATH=${PYTHON_DIR}/lib/pkgconfig --enable-optimizations
 
 # Build Python3
@@ -91,13 +86,7 @@ export MPICXX=mpiicpc
 export MPIF90=mpiifort
 
 module unload python/2.7.8
-# remove???
-#unset PETSC_ARCH
 
 export INTEL_LICENSE_FILE=/cm/shared/licenses/intel/
-### This line doesn't work, python3 complains about not being able to find encodings module, but then runs fine in terminal after
-python3 firedrake-install --mpicc=mpiicc --mpicxx=mpiicpc --mpif90=mpiifort --no-package-manager --disable-ssh --honour-petsc-dir
 
-# Add paths to .bashrc (only do this once!)
-# echo PETSC_DIR=${MAIN}/petsc >> ~/.bashrc
-# echo PATH="${PYTHON_DIR}/bin:$PATH" >> ~.bashrc
+python3 firedrake-install --mpicc=mpiicc --mpicxx=mpiicpc --mpif90=mpiifort --no-package-manager --disable-ssh --honour-petsc-dir
