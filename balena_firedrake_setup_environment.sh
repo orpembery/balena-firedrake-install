5b3560da322be0ffffed54042a94729e10cea84a
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-12-14 09:06:26 +0000

    Added module purge and zlib to setup script in real.

--- balena_firedrake_setup_environment.sh
+++ balena_firedrake_setup_environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Default modules to load
+module purge
 module load git/2.5.1
 module load intel/mpi/64/18.0.128
 module load intel/mkl/64/11.3.3
@@ -9,6 +10,8 @@ module load htop
 module load cmake/3.5.2
 module load intel/compiler/64/18.0.128
 module load slurm
+module load zlib
+
 
 MAIN=/home/s/orp20/scratch/firedrake
 
