dc31a5b931d83c519d050305772aa85d02178d67
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-04 09:38:19 +0100

    Added hack to make mesh generation time work.

---
+++ firedrake_complex_hacks/balena_hacks.py
@@ -0,0 +1,51 @@
+import ctypes
+import os
+from os import environ as env
+def fix_mesh_generation_time():
+    """For some reason, the current install script for Complex on Balena
+    (the University of Bath's high performance computer) takes a very
+    long time to generate meshes. This code appears to fix the problem,
+    and was suggested my Michal Habera -
+    https://wwwen.uni.lu/recherche/fstc/
+    research_unit_in_engineering_sciences_rues/members/michal_habera.
+
+    Parameters:
+
+    :param:firedrake_install_path: A string giving the path to the
+    current firedrake install. Must end in '/'.
+    """
+
+    petsc_dir = os.environ.get('PETSC_DIR', None)
+
+    petsc_arch = os.environ.get('PETSC_ARCH', None)
+    
+    libpetsc_path = os.path.join(petsc_dir, petsc_arch, 'lib', 'libpetsc.so')
+
+    petsc = ctypes.CDLL(libpetsc_path)
+
+    # Do not check validity of address before dereferencing pointers
+
+    petsc.PetscCheckPointerSetIntensity(0)
+
+# def get_petsc_dir():
+#     try:
+#         petsc_arch = env.get('PETSC_ARCH', '')
+#         petsc_dir = env['PETSC_DIR']
+#         if petsc_arch:
+#             return (petsc_dir, env.join(petsc_dir, petsc_arch))
+#         return (petsc_dir,)
+#     except KeyError:
+#         try:
+#             import petsc
+#             return (petsc.get_petsc_dir(), )
+#         except ImportError:
+#             sys.exit("""Error: Could not find PETSc library.
+# Set the environment variable PETSC_DIR to your local PETSc base
+# directory or install PETSc from PyPI as described in the manual:
+# http://firedrakeproject.org/obtaining_pyop2.html#petsc
+# """)
+
+# Above code taken from
+# Firedrake project at
+# https://github.com/firedrakeproject/firedrake/blob/46681927e3194d587e09684a49a952ae3cc68269/setup.py,
+# used under LGPLv3
